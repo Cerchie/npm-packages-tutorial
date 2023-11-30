40321d8305914ea6b9478e7d7f2acba07306ed34
Cerchie <luciacerchie@gmail.com>
2023-11-30 11:54:21 -0800

    spring func

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+// Get input values
+function updateValue(){
+    const mass =  parseFloat(document.getElementById("mass").value);
+    const stiffness = parseFloat(document.getElementById("stiffness").value);
+    const damping = parseFloat(document.getElementById("damping").value);
+    const velocity =  parseFloat(document.getElementById("velocity").value);
+  
+    console.log(mass, stiffness)
+    return {mass: mass, stiffness: stiffness, damping: damping, velocity: velocity}
+}
 
+function spring(){
 
-function spring() {
-    // Get input values
-    const mass = parseFloat(document.getElementById('mass').value);
-    const stiffness = parseFloat(document.getElementById('stiffness').value);
-    const damping = parseFloat(document.getElementById('damping').value);
-    const velocity = parseFloat(document.getElementById('velocity').value);
+    const mass = updateValue().mass;
+    const stiffness = updateValue().stiffness;
+    const damping = updateValue().damping;
+    const velocity = updateValue().velocity;
+    console.log('spring',mass, stiffness)
 
     // Validate input values
     if (isNaN(mass) || isNaN(stiffness) || isNaN(damping) || isNaN(velocity)) {
       alert('Please enter valid numeric values for all fields.');
-      return;
     }
 
-    anime({
+    var animation = anime({
         targets: '.thingy',
         translateX: 200,
         direction: 'alternate',
@@ -21,5 +30,8 @@ function spring() {
         easing: `spring(${mass}, ${stiffness}, ${damping}, ${velocity}`
       })
 
-  }
+    document.querySelector('.play').onclick = animation.play;
+    document.querySelector('.pause').onclick = animation.pause;
+
+    }
 
